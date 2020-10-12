acd8c220b3fa90f66dfdd05900d457e71936ed73
Emily <hoppe23@up.edu>
2020-10-12 14:04:17 -0700

    Card class, draw methods, and discard method added

--- app/src/main/java/com/example/phase10gamestate/Card.java
+++ app/src/main/java/com/example/phase10gamestate/Card.java
@@ -1,4 +1,37 @@
 package com.example.phase10gamestate;
 
 public class Card {
+    private int number; //1-12 number
+    private int color; // 1-4 to represent the 4 colors
+    //wild cards are 0,0
+    //skip cards are -1,-1
+
+    public Card(int n, int c){
+        this.number = n;
+        this.color = c;
+    }
+
+    public void setNumber(int n){
+        this.number = n;
+    }
+    public int getNumber(){
+        return this.number;
+    }
+
+    public void setColor(int c){
+       this.color = c;
+    }
+    public int getColor(){
+        return this.color;
+    }
+
+
+    public boolean isWild(){
+        if(this.number == 0 && this.color == 0) return true;
+        else return false;
+    }
+    public boolean isSkip(){
+        if(this.number == -1 && this.color == -1) return true;
+        else return false;
+    }
 }
--- app/src/main/java/com/example/phase10gamestate/Phase10GameState.java
+++ app/src/main/java/com/example/phase10gamestate/Phase10GameState.java
@@ -1,20 +1,117 @@
+//Need to add header comment to every java file
 package com.example.phase10gamestate;
+
+import java.util.ArrayList;
+import java.util.Stack;
 
 public class Phase10GameState {
 
     //Resources
-    private int turnId; //1 or 2
-    private Card [] player1Hand;
-    private Card [] player2Hand;
-    private Card [] Player1PhaseContent;
-    private Card [] Player2PhaseContent;
+    private int turnId; //1 or 2 based on id of player turn
+    private ArrayList<Card> player1Hand; //Player hands start at 10 cards
+    private ArrayList<Card> player2Hand;
+    private ArrayList<Card> player1PhaseContent;
+    private ArrayList<Card> player2PhaseContent;
+    private ArrayList<Card> drawPile;
+    private Stack<Card> discardPile;
     private boolean player1HasPhased;
     private boolean player2HasPhased;
-    private int player1Score;
+    private boolean playerHasDrawn; //Only one draw allowed per round
+    private int goesFirst; //Player Id of who goes first, alternates each round
+    private int player1Score; //Lower scores are better
     private int player2Score;
     private int player1Phase;
     private int player2Phase;
     private int hasGoneOut; //set to zero until a player goes out, then set to player Id
 
+    /**
+     * attempts to draw a face down card from the draw pile and add it to the player's hand
+     * player does not know what the card is until they draw it
+     *
+     * @param playerId id of the player attempting to draw, this param could be replaced by a player object later on,
+     *                 in which case a .getId() call would be added to the method
+     * @return true if the action was successful, else will return false
+     */
+    public boolean drawFaceDown(int playerId){
 
+        if(playerId != this.turnId ||this.drawPile.size() == 0 || this.hasGoneOut == playerId || this.playerHasDrawn) return false;
+
+        Card drawn = this.drawPile.remove(0); //Remove top card from draw pile
+        this.playerHasDrawn = true;
+
+        //determine which player hand it goes to
+        if(playerId == 1){
+            this.player1Hand.add(drawn); //add to hand
+            return true;
+        }
+        else if(playerId == 2){
+            this.player2Hand.add(drawn); //add to hand
+            return true;
+        }
+
+        else return false;
+    }
+
+    /**
+     * attempts to draw a face up card from the discard pile and add it to the player's hand
+     * the card drawn will be the last card discarded onto the pile, so player knows which card they are drawing
+     *
+     * @param playerId id of the player attempting to draw, this param could be replaced by a player object later on,
+     *                 in which case a .getId() call would be added to the method
+     * @return true if the action was successful, else will return false
+     */
+    public boolean drawFaceUp(int playerId){
+
+        if(playerId != this.turnId ||this.discardPile.size() == 0 || this.hasGoneOut == playerId || this.playerHasDrawn) return false;
+
+        Card drawn = this.discardPile.pop(); //remove top card from discard
+        this.playerHasDrawn = true;
+
+        //determine which player hand it goes to
+        if(playerId == 1){
+            this.player1Hand.add(drawn); //add to hand
+            return true;
+        }
+        else if(playerId == 2){
+           this.player2Hand.add(drawn); //add to hand
+            return true;
+        }
+        else return false;
+    }
+
+    /**
+     * attempts to discard a card from player hand to the discard pile
+     *
+     * @param playerId id of the player attempting to discard, this param could be replaced by a player object later on,
+     *                 in which case a .getId() call would be added to the method
+     * @param cardLoc the location in player hand of the card being discarded
+     * @return true if the action was successful, else will return false
+     */
+    public boolean discard(int playerId, int cardLoc){
+
+        if(playerId != this.turnId ||this.hasGoneOut == playerId || cardLoc < 0) return false;
+
+        //determine which player is discarding
+        if(playerId == 1){
+            if(this.player1Hand.size() < cardLoc) return false;
+
+            discardPile.push(this.player1Hand.remove(cardLoc)); //discard to discard pile
+
+            if(this.player1Hand.size() == 0)  this.hasGoneOut = playerId; //If a player's hand is empty, the other player gets one turn before round ends
+            if(!discardPile.peek().isSkip()) this.turnId = 2; //Skips in 2 player mode allow current player to take 2 back-to-back turns
+
+            return true;
+        }
+        else if(playerId == 2){
+            if(this.player2Hand.size() < cardLoc) return false;
+
+            discardPile.push(this.player2Hand.remove(cardLoc)); //discard to discard pile
+
+            if(this.player2Hand.size() == 0)  this.hasGoneOut = playerId;
+            if(!discardPile.peek().isSkip()) this.turnId = 1;
+
+            return true;
+        }
+        else return false;
+    }
 }
