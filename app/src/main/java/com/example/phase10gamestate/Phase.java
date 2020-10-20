fcaf068b99aadc22e3e6a3302428882d520af4fb
A Molina <70313285+molinaa451@users.noreply.github.com>
2020-10-19 21:45:03 -0600

    checkHitValid and comments in Phase class

--- app/src/main/java/com/example/phase10gamestate/Phase.java
+++ app/src/main/java/com/example/phase10gamestate/Phase.java
@@ -39,7 +39,7 @@ public class Phase {
     //needs constructor
 
     /** checks if the play can play a phase, first by seeing what phase
-     * the player is on, then my referencing two different methods that checks
+     * the player is on, then by referencing two different methods that checks
      * each card to make sure the play can hit */
     public boolean checkPhase(int playerPhase, ArrayList<Card> phaseContent, int playerNum) {
         Card[] sorted = sortCards(phaseContent);
@@ -69,6 +69,14 @@ public class Phase {
                 return false;
         }
     }
+
+    /* Each boolean method checks for a specific phase as specified
+        in the name, except for 8, which is a simple case that only
+        checks for 7 cards of 1 color, and sets whatever parameter is not used to null
+        --i.e. if it calls for sets but no runs, then runs for the player is set to null
+
+     */
+
 
     public boolean checkIfPhaseOne(Card[] phasecontent, int playerNum){ //2 sets of 3
         if(isSet(phasecontent,3, playerNum,1) != null) {
@@ -186,7 +194,15 @@ public class Phase {
         }
         return false;
     }
+    // end of checkIfPhases
 
+    /* isSet checks to find the same number within the phaseContent of a player,
+       then sorts those that are the same number into a temporary array
+       which is then assigned to a set for a specific player whether that set
+       be the first or second set, depending on the checkIfPhase()
+       parameters. The remaining not used are stored in a seperate array.
+
+     */
     public Card[] isSet(Card[] checkForSet, int size, int playerNum, int setNum){
         Card[] temp = new Card[size];
         Card[] notInSet = new Card[checkForSet.length-size];
@@ -234,6 +250,11 @@ public class Phase {
         return null;
     }
 
+    /* isRun sorts through to play chronologically and stores them as an array for
+       specific player, and those that cannot be sorted chronilogically are stored
+       in an array for those not in a run
+
+     */
     public Card[] isRun(Card[] checkForRun, int size, int playerNum){
         Card[] temp = new Card[size];
         Card[] notInRun = new Card[checkForRun.length-size];
@@ -268,6 +289,14 @@ public class Phase {
         return null;
     }
 
+
+    /* isColorGroup check runs through the cards and checks to see how many of the
+       cards are the same card, checkForColor parameter stores the color in an array,
+       the size indicates how many need to be the same color, and the playerNum
+       allows us to designate to which Card set for which player needs to assign the remaining
+       unused cards and the used cards to which color set
+
+     */
     public Card[] isColorGroup(Card[] checkForColor, int size, int playerNum){
         Card[] temp = new Card[size];
         Card[] notInColor = new Card[checkForColor.length-size];
@@ -301,6 +330,11 @@ public class Phase {
         }
         return null;
     }
+
+    /* sortCards sorts through the cards of phaseContent so that
+        for the isRun, isSet, isColorGroup methods the cards can be
+        sorted from smallest to largest via card array as opposed to an ArrayList
+     */
     public Card[] sortCards(ArrayList<Card> attempt){
         Card[] arr = new Card[attempt.size()];
         int x = 0;
@@ -322,51 +356,87 @@ public class Phase {
         return arr;
     }
 
+    /* checkHitValid checks if the selected card is valid
+
+     */
     protected boolean checkHitValid(Card selectedCard, ArrayList<Card> phaseContent, int playerNum) {
         if (playerNum == 1) {
+
+            //runs
             Card[] tempPlay1Run = new Card[play1Run.length+1];
             for(int i = 0; i<play1Run.length; i++){
                tempPlay1Run[i] = play1Run[i];
             }
             tempPlay1Run[play1Run.length] = selectedCard;
+            if(isRun(tempPlay1Run, size, playerNum)) return true;
+            else return false;
 
-            Card[] tempPlay1Set = new Card[play1Set.length+1];
-            ArrayList<Card> tempPlay1Set = play1Set;
-            tempPlay1Set.add(selectedCard);
-            ArrayList<Card> tempPlay1Color = play1Color;
-            tempPlay1Color.add(selectedCard);
-            if (checkIfInARow(tempPlay1Run)) {
-                play1Run = tempPlay1Run;
-                return true;
-            } else if (checkForEqualNumbers(tempPlay1Set)) {
-                play1Set = tempPlay1Set;
-                return true;
-            } else if (checkSameColors(tempPlay1Color)) {
-                play1Color = tempPlay1Color;
-                return true;
+            //sets
+            Card[] tempPlay1Set1 = new Card[play1Set1.length+1];
+            for(int i=0; i<play1Set1.length; i++) {
+                tempPlay1Set1 = play1Set1[i] ;
             }
-            return true;
+            tempPlay1Set1[play1Set1.length]=selectedCard;
+
+            Card[] tempPlay1Set2 = new Card[play1Set2.length+1];
+            for(int i=0; i<play1Set2.length; i++) {
+                tempPlay1Set2 = play1Set2[i] ;
+            }
+            tempPlay1Set2[play1Set2.length]=selectedCard;
+
+            if(isSet(tempPlay1Set1, size, playNum,1)) return true;
+            else if(isSet(tempPlay1Set2, size, playerNum), 2)) return true;
+            else return false;
+
+            // colors
+
+            Card[] tempPlay1Color = new Card[play1Color.length+1];
+            for(int i=0; i<play1Color.length; i++) {
+                tempPlay1Color = play1Color[i];
+                }
+            tempPlay1Color[play1Color.lenght] = selectedCard;
+
+            if(isColorGroup(tempPlay1Color, size, playerNum)) return true;
+            else return false;
 
         } else if (playerNum == 2) {
-            ArrayList<Card> tempPlay2Run = play2Run;
-            tempPlay2Run.add(selectedCard);
-            ArrayList<Card> tempPlay2Set = play2Set;
-            tempPlay2Set.add(selectedCard);
-            ArrayList<Card> tempPlay2Color = play2Color;
-            tempPlay2Color.add(selectedCard);
-            if (checkIfInARow(tempPlay2Run)) {
-                play2Run = tempPlay2Run;
-                return true;
-            } else if (checkForEqualNumbers(tempPlay2Set)) {
-                play1Set = tempPlay2Set;
-                return true;
-            } else if (checkSameColors(tempPlay2Color)) {
-                play2Color = tempPlay2Color;
-                return true;
+            Card[] tempPlay2Run = new Card[play2Run.length+1];
+            for(int i = 0; i<play2Run.length; i++){
+                tempPlay2Run[i] = play2Run[i];
             }
-            return true;
-        }
-        return false;
+            tempPlay2Run[play2Run.length] = selectedCard;
+
+            if(isRun(tempPlay2Run, size, playerNum)) return true;
+            else return false;
+
+            //sets
+            Card[] tempPlay2Set1 = new Card[play2Set1.length+1];
+            for(int i=0; i<play2Set1.length; i++) {
+                tempPlay1Set1 = play2Set1[i] ;
+            }
+            tempPlay2Set1[play2Set1.length]=selectedCard;
+
+            Card[] tempPlay2Set2 = new Card[play2Set2.length+1];
+            for(int i=0; i<play2Set2.length; i++) {
+                tempPlay2Set2 = play2Set2[i] ;
+            }
+            tempPlay2Set2[play2Set2.length]=selectedCard;
+
+            if(isSet(tempPlay2Set1, size, playNum,1)) return true;
+            else if(isSet(tempPlay2Set2, size, playerNum), 2)) return true;
+            else return false;
+
+            // colors
+
+            Card[] tempPlay2Color = new Card[play2Color.length+1];
+            for(int i=0; i<play2Color.length; i++) {
+                tempPlay2Color = play2Color[i];
+            }
+            tempPlay2Color[play2Color.lenght] = selectedCard;
+
+            if(isColorGroup(tempPlay2Color, size, playerNum)) return true;
+            else return false;
     }
 
+    }//checkHitValid end
 }
